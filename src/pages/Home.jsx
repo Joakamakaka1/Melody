02bd8572310117af76c6e1a63d0be60ac9ab27bb
Joakamakaka1 <joaquinfutbolero11@gmail.com>
2025-02-18 23:56:49 +0100

    Prueba accesibilidad

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,16 +17,19 @@ export default function Home() {
         <p className="text-gray-400 mt-4">
           Explora y guarda tus canciones favoritas.
         </p>
-        <button className="mt-6 px-6 py-3 bg-blue-500 rounded-lg hover:bg-blue-600 cursor-pointer">
+        <button
+          className="mt-6 px-6 py-3 bg-blue-500 rounded-lg hover:bg-blue-600 cursor-pointer"
+          aria-label="Empezar ahora"
+        >
           Empezar ahora
         </button>
       </section>
 
       {/* Destacados */}
-      <section className="container mx-auto py-12 px-6 ">
+      <section className="container mx-auto py-12 px-6">
         <h3 className="text-2xl font-semibold pb-2">Playlist Destacadas</h3>
         <hr className="border-t border-gray-700 mb-5" />
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-6 ">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
           {[1, 2, 3, 4].map((_, index) => (
             <div
               key={index}
@@ -35,56 +38,33 @@ export default function Home() {
               <div className="h-75 bg-gray-700 rounded-md">
                 <img
                   src="img/album3.webp"
-                  alt="iamgenPlaylist"
+                  alt={`Playlist ${index + 1}`}
                   className="w-full h-full object-cover"
                 />
               </div>
-              <h4 className="mt-4 text-lg font-semibold">
-                Usuario {index + 1}
-              </h4>
+              <h4 className="mt-4 text-lg font-semibold">Usuario {index + 1}</h4>
               <p className="text-gray-400">Playlist {index + 1}</p>
             </div>
           ))}
         </div>
       </section>
 
-      {/* ¿Qué puedes hacer en Melody? */}
+      {/* Sección de funcionalidades */}
       <section className="container mx-auto py-12 px-6">
         <h3 className="text-2xl font-semibold mb-6">
           ¿Qué puedes hacer en Melody?
         </h3>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {[
-            {
-              icon: <FaHeadphones />,
-              text: "Lleva un registro de todas las canciones que escuchas y descubre nuevas.",
-            },
-            {
-              icon: <FaHeart />,
-              text: "Da 'me gusta' a canciones, playlists y álbumes para guardarlos en tu colección.",
-            },
-            {
-              icon: <FaPen />,
-              text: "Escribe y comparte reseñas de canciones y álbumes con otros usuarios.",
-            },
-            {
-              icon: <FaStar />,
-              text: "Califica canciones y álbumes en una escala de 5 estrellas y comparte tu opinión.",
-            },
-            {
-              icon: <FaCalendarAlt />,
-              text: "Lleva un diario de tu experiencia musical y mira cómo evoluciona tu gusto.",
-            },
-            {
-              icon: <FaList />,
-              text: "Crea listas de reproducción y compártelas con amigos o la comunidad.",
-            },
+            { icon: <FaHeadphones />, text: "Lleva un registro de todas las canciones que escuchas y descubre nuevas." },
+            { icon: <FaHeart />, text: "Da 'me gusta' a canciones, playlists y álbumes para guardarlos en tu colección." },
+            { icon: <FaPen />, text: "Escribe y comparte reseñas de canciones y álbumes con otros usuarios." },
+            { icon: <FaStar />, text: "Califica canciones y álbumes en una escala de 5 estrellas y comparte tu opinión." },
+            { icon: <FaCalendarAlt />, text: "Lleva un diario de tu experiencia musical y mira cómo evoluciona tu gusto." },
+            { icon: <FaList />, text: "Crea listas de reproducción y compártelas con amigos o la comunidad." },
           ].map((item, index) => (
-            <Link to={"#"}>
-              <div
-                key={index}
-                className="bg-gray-800 p-10 rounded-lg shadow-lg flex items-center space-x-4 cursor-pointer hover:bg-gray-700 transition-colors duration-300"
-              >
+            <Link to="#" key={index} aria-label={item.text}>
+              <div className="bg-gray-800 p-10 rounded-lg shadow-lg flex items-center space-x-4 cursor-pointer hover:bg-gray-700 transition-colors duration-300">
                 <div className="text-blue-400 text-4xl">{item.icon}</div>
                 <p className="text-gray-400 text-xl">{item.text}</p>
               </div>
@@ -93,55 +73,37 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Popular Reviews This Week */}
+      {/* Reseñas populares */}
       <section className="container mx-auto py-12 px-6">
-        <h3 className="text-2xl font-semibold pb-2">
-          Popular Reviews This Week
-        </h3>
+        <h3 className="text-2xl font-semibold pb-2">Popular Reviews This Week</h3>
         <hr className="border-t border-gray-700 mb-5" />
         <div className="space-y-6">
           {[1, 2, 3].map((_, index) => (
-            <div
-              key={index}
-              className="bg-gray-800 p-6 rounded-lg shadow-lg flex items-center space-x-6"
-            >
-              <Link to={"#"}>
+            <div key={index} className="bg-gray-800 p-6 rounded-lg shadow-lg flex items-center space-x-6">
+              <Link to="#" aria-label={`Ver detalles de la Playlist ${index + 1}`}>
                 <div className="h-32 w-32 bg-gray-700 rounded-md">
                   <img
                     src="img/album1.webp"
-                    alt="imagenPlaylist"
+                    alt={`Imagen de la Playlist ${index + 1}`}
                     className="w-full h-full object-cover"
                   />
                 </div>
               </Link>
               <div className="flex-1">
-                <Link to={"#"}>
-                  <h4 className="text-lg font-semibold text-white">
-                    Playlist {index + 1}
-                  </h4>
+                <Link to="#" aria-label={`Ver detalles de la Playlist ${index + 1}`}>
+                  <h4 className="text-lg font-semibold text-white">Playlist {index + 1}</h4>
                 </Link>
-                <Link to={"#"}>
-                  <p className="text-gray-400">
-                    Comentario de Usuario {index + 1}
-                  </p>
-                </Link>
+                <p className="text-gray-400">Comentario de Usuario {index + 1}</p>
                 <div className="flex items-center mt-2">
-                  {/* Estrellas */}
                   {[...Array(5)].map((_, i) => (
-                    <FaStar
-                      key={i}
-                      className={`text-yellow-400 ${
-                        i < 4 ? "text-yellow-400" : "text-gray-500"
-                      }`}
-                    />
+                    <FaStar key={i} className={`text-yellow-400 ${i < 4 ? "text-yellow-400" : "text-gray-500"}`} />
                   ))}
                 </div>
                 <p className="text-gray-400 mt-4">
-                  "Este es un comentario sobre la playlist. Es genial y tiene
-                  muchas canciones buenas!"
+                  "Este es un comentario sobre la playlist. Es genial y tiene muchas canciones buenas!"
                 </p>
                 <div className="mt-4 flex items-center space-x-2">
-                  <FaHeart className="text-gray-400 hover:text-red-500 cursor-pointer" />
+                  <FaHeart className="text-gray-400 hover:text-red-500 cursor-pointer" aria-label="Dar me gusta" />
                   <span className="text-gray-400">50 Likes</span>
                 </div>
               </div>
